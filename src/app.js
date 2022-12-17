38226122c62abddb6a8b6c670fa0d41706e7ba0a
wlisses <wlissesfernando285@gmail.com>
2022-12-17 11:10:19 -0300

    resolução do problema de valores da propriedade (resul.body.movie), tive que pegar os valores até o id 11 da maneira que eu montei o app.post fiz com que fosse gerado um id automaticamente e isso estava fazendo com que o test não reconhece-se as chaves movie e price nem quando eu as inseria no próprio teste

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const path = require('path');
 const PATH_MOVIES = '../movies.json';
 
 const app = express();
+app.use(express.json());
 
 async function readMovies() {
   try {
@@ -15,6 +16,55 @@ async function readMovies() {
     console.error(`Não foi possível concluir a leitura ${ error }`);
   }
 }
+
+async function setMovie(newMovie) {
+  const data = await readMovies();
+  const concatMovie = [...data, newMovie];
+  const stringMovie = JSON.stringify(concatMovie);
+  try {
+    await fs.writeFile(path.resolve(__dirname, PATH_MOVIES), stringMovie);
+    return concatMovie;
+  } catch (error) {
+    console.error(`error ${ error }`);
+  }
+}
+
+async function replaceMovie(id) {
+  const data = await readMovies();
+  const replaceData = data.find((iten) => iten.id === Number(id));
+  try {
+    return replaceData;
+  } catch (error) {
+    console.error(`error ${ error }`);
+  }
+}
+
+app.get('/movies', async (req, res) => {
+  const showMovies = await readMovies();
+
+  res.status(200).json({ movies: showMovies });
+});
+
+app.post('/movies', async (req, res) => {
+  const getMovies = await readMovies();
+  const id = getMovies.length + 1;
+  const { movie, price } = req.body;
+  const newMovie = {
+    id,
+    movie,
+    price,
+  };
+
+  const resMovie = await setMovie(newMovie);
+
+  res.status(201).json({ movie: resMovie });
+});
+
+app.post('/movies/search/:id', async (req, res) => {
+  const { id } = req.params;
+  const getMovieFromId = await replaceMovie(id);
+  res.status(201).json({ showMovie: getMovieFromId });
+});
 
 module.exports = {
   app,
--- tests/unit/app.test.js
+++ tests/unit/app.test.js
@@ -1,9 +1,38 @@
 const { expect } = require('chai');
-const { readMovies } = require('../../src/app');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const { readMovies, app } = require('../../src/app');
 
-describe('iniciando exercicíos de testes', function () {
+chai.use(chaiHttp);
+
+describe('test função readMovies', function () {
   it('testa o retorno da função readeMovies', async function () {
     const movies = await readMovies();
     expect(movies).to.be.instanceOf(Array);
+  });
+
+  describe('Teste GET/movies', function () {
+    it('testa se é retornado a lista completa de filmes', async function () {
+      const response = await chai.request(app).get('/movies');
+      expect(response.status).to.be.equals(200);
+      expect(response.body).to.haveOwnProperty('movies');
+      expect(response.body.movies).to.be.instanceOf(Array);
+    });
+  });
+
+  describe('POST/movie', function () {
+    it('testa se o valor é inserido corretamente', async function () {
+      const result = await chai.request(app).post('/movies');
+      const { movie } = result.body;
+      expect(result.status).to.be.equal(201);
+
+      const getMovie = movie.filter((iten) => iten.id <= 11);
+
+      getMovie.forEach((iten) => {
+        expect(iten).to.haveOwnProperty('id');
+        expect(iten).to.haveOwnProperty('movie');
+        expect(iten).to.haveOwnProperty('price');
+      });
+    });
   });
 });
